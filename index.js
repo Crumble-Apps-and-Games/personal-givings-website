3d2a0ac2a44226a71e98538aa7a3973629c95ce9
Phyrik <34688504+Phyrik@users.noreply.github.com>
2020-07-14 18:30:30 +0100

    firebase and express setup

---
+++ index.js
@@ -0,0 +1,24 @@
+var firebase = require("firebase/app")
+
+require("firebase/auth")
+require("firebase/firestore")
+
+var firebaseConfig = {
+    apiKey: "AIzaSyD8tpGXe_T2dBda_nmDvw-yBU2Nei_FnQQ",
+    authDomain: "hhpc-weekly-givings-website.firebaseapp.com",
+    databaseURL: "https://hhpc-weekly-givings-website.firebaseio.com",
+    projectId: "hhpc-weekly-givings-website",
+    storageBucket: "hhpc-weekly-givings-website.appspot.com",
+    messagingSenderId: "861520292247",
+    appId: "1:861520292247:web:4392f3600045bdeac7fcd2",
+    measurementId: "G-0ZX7NWDH4Z"
+}
+
+firebase.initializeApp(firebaseConfig)
+
+var database = firebase.database()
+
+var express = require("express")
+var expressApp = express()
+
+expressApp.use('/pages', express.static(path.join(__dirname, 'public')))
